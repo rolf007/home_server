191d092b0252f5b7531122e6c6ba180a01147817
rolf007 <rolf@asmund.dk>
2018-12-05 20:27:47 +0100

    script for starting and stopping services

---
+++ home_server.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if [ "$1" == "start" ]; then
+	screen -dmS sms_portal "sms_portal/sms_portal.py"
+	screen -dmS emoji "emoji/emoji.py"
+	screen -ls
+elif [ "$1" == "stop" ]; then
+	for line in $(screen -ls | grep "(Detached)" | sed -e 's/\s*\(\S\+\)\s.*/\1/'); do
+		echo stopping $line
+		pid=$(echo "$line" | sed -e 's/^\s*\([0-9]\+\)\..*$/\1/g')
+		screen -S $pid -X quit
+	done
+	screen -ls
+fi
+# list all:
+# screen -ls
+
+# reattach example:
+# screen -r emoji
+
