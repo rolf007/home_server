895754bba5d4ef55a52ff423ecb183c6145cec3e
rolf007 <rolf@asmund.dk>
2019-01-12 00:29:40 +0100

    more tags in scan_collection.py

--- music_server/scan_collection.py
+++ music_server/scan_collection.py
@@ -9,24 +9,49 @@ import os
 import subprocess
 import sys
 
-from mkdirp import mkdirp
-
 home_server_root = os.path.split(sys.path[0])[0]
 home_server_config = os.path.join(os.path.split(home_server_root)[0], "home_server_config", os.path.split(sys.path[0])[1])
+
+sys.path.append(os.path.join(home_server_root, "utils"))
+
+from mkdirp import mkdirp
 
 def scan_collection(scan_path):
     music_collection = {}
     directory = os.fsencode(scan_path)
+    total_num_files = 0
     for root, dirs, files in os.walk(directory):
         for file in files:
+            total_num_files = total_num_files + 1
+    file_num = 0
+    pct = ""
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_num = file_num + 1
+            new_pct = "%d%%" % (file_num*100/total_num_files)
+            if new_pct != pct:
+                pct = new_pct
+                print(pct)
             if file.endswith(b'.mp3'):
                 url = os.path.join(root, file).decode('ascii')
 
                 audiofile = eyed3.load(url)
                 if audiofile and audiofile.tag:
-                    print("found : %s - %s (%s)" % ( ascii(audiofile.tag.artist), ascii(audiofile.tag.title), url))
-                    music_collection[url] = {"title":audiofile.tag.title, "artist":audiofile.tag.artist}
+                    #print("found : %s - %s (%s)" % ( ascii(audiofile.tag.artist), ascii(audiofile.tag.title), url))
+                    music_collection[url] = {
+                        "album":audiofile.tag.album,
+                        "artist":audiofile.tag.artist,
+                        "bpm":audiofile.tag.bpm,
+                        "genre":str(audiofile.tag.genre),
+                        "release_date":str(audiofile.tag.release_date),
+                        "title":audiofile.tag.title,
+                        "track_num":audiofile.tag.track_num
+                        }
     return music_collection
+#'getBestDate',
+
+#!music_server/scan_collection.py /mnt/usb/Albums\ -\ Alfabetisk/M/Metallica/Metallica-And_Justice_For_All-1988-INT-DDZ/
+#!music_server/scan_collection.py /mnt/usb/Albums\ -\ Alfabetisk/
 
 
 parser = argparse.ArgumentParser()
