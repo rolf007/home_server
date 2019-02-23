77d8eb5a0d59356bc70a5c9cae064f629dc81884
rolf007 <rolf@asmund.dk>
2019-02-23 12:40:32 +0100

    very primitive Web App

---
+++ web_app/web_app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+from aiohttp import web
+
+async def handle(request):
+    text = """
+<iframe width="10" height="10" border="0" name="dummyframe" id="dummyframe"></iframe>
+
+<form action="http://192.168.0.100:5100/suresms" target="dummyframe">
+
+  <input type="hidden" name="receivedutcdatetime" value="time" />
+  <input type="hidden" name="receivedfromphonenumber" value="12345678" />
+  <input type="hidden" name="receivedbyphonenumber" value="87654321" />
+  <input type="hidden" name="body" value="p .r ulf lundell" />
+  <button type="submit">Ulf Lundell</button>
+</form>
+
+<form action="http://192.168.0.100:5100/suresms" target="dummyframe">
+
+  <input type="hidden" name="receivedutcdatetime" value="time" />
+  <input type="hidden" name="receivedfromphonenumber" value="12345678" />
+  <input type="hidden" name="receivedbyphonenumber" value="87654321" />
+  <input type="hidden" name="body" value="radio p1" />
+  <button type="submit">P1</button>
+</form>
+
+<form action="http://192.168.0.100:5100/suresms" target="dummyframe">
+
+  <input type="hidden" name="receivedutcdatetime" value="time" />
+  <input type="hidden" name="receivedfromphonenumber" value="12345678" />
+  <input type="hidden" name="receivedbyphonenumber" value="87654321" />
+  <input type="hidden" name="body" value="radio p2" />
+  <button type="submit">P2</button>
+</form>
+
+<form action="http://192.168.0.100:5100/suresms" target="dummyframe">
+
+  <input type="hidden" name="receivedutcdatetime" value="time" />
+  <input type="hidden" name="receivedfromphonenumber" value="12345678" />
+  <input type="hidden" name="receivedbyphonenumber" value="87654321" />
+  <input type="hidden" name="body" value="radio p3" />
+  <button type="submit">P3</button>
+</form>
+
+<form action="http://192.168.0.100:5100/suresms" target="dummyframe">
+
+  <input type="hidden" name="receivedutcdatetime" value="time" />
+  <input type="hidden" name="receivedfromphonenumber" value="12345678" />
+  <input type="hidden" name="receivedbyphonenumber" value="87654321" />
+  <input type="hidden" name="body" value="radio 24syv" />
+  <button type="submit">24syv</button>
+</form>
+
+"""
+    headers = {'content-type': 'text/html'}
+    return web.Response(headers=headers, text=text)
+
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/{name}', handle)])
+
+web.run_app(app)
+
